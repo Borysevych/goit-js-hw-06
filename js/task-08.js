ebb903f7b081fcc02ade3be993884875cca061a7
Borysevych <borysevych.rus@gmail.com>
2023-02-21 21:56:19 +0200

    HW 6 Final
    made HW6

--- js/task-08.js
+++ js/task-08.js
@@ -5,10 +5,15 @@ formRef.addEventListener('submit', (e) => {
 
     const {
         elements: { email, password } } = e.currentTarget;
+    const formData = {
+        Login: email.value,
+        Password: password.value
+    };
+    
     if (email.value === '' || password.value === '') {
         return alert('Всі поля повинні бути заповнені')
     } else {
-        console.log(`Login: ${email.value}, Password: ${password.value}`);
+        console.log(formData);
         e.currentTarget.reset();
     }
 });
